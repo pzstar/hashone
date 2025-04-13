ee5d35e92a504de6d0208ee22fd8e71b084432bf
Prabin Dangol <neprabin@gmail.com>
2025-04-13 17:35:23 +0545

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -49,7 +49,7 @@ if (!function_exists('hashone_entry_footer')) :
         if ('post' == get_post_type()) {
             /* translators: used between list items, there is a space after the comma */
             $categories_list = get_the_category_list(', ');
-            if ($categories_list && hashone_categorized_blog()) {
+            if ($categories_list) {
                 printf('<span class="cat-links">' . esc_html__('Posted in %1$s', 'hashone') . '</span>', $categories_list); // WPCS: XSS OK.
             }
 
@@ -81,54 +81,10 @@ if (!function_exists('hashone_entry_category')) :
         if ('post' == get_post_type()) {
             /* translators: used between list items, there is a space after the comma */
             $categories_list = get_the_category_list(', ');
-            if ($categories_list && hashone_categorized_blog()) {
+            if ($categories_list) {
                 echo '<i class="fa fa-bookmark"></i>' . $categories_list; // WPCS: XSS OK.
             }
         }
     }
 
 endif;
-
-/**
- * Returns true if a blog has more than 1 category.
- *
- * @return bool
- */
-function hashone_categorized_blog() {
-    if (false === ( $all_the_cool_cats = get_transient('hashone_categories') )) {
-        // Create an array of all the categories that are attached to posts.
-        $all_the_cool_cats = get_categories(array(
-            'fields' => 'ids',
-            'hide_empty' => 1,
-            // We only need to know if there is more than one category.
-            'number' => 2,
-        ));
-
-        // Count the number of categories that are attached to the posts.
-        $all_the_cool_cats = count($all_the_cool_cats);
-
-        set_transient('hashone_categories', $all_the_cool_cats);
-    }
-
-    if ($all_the_cool_cats > 1) {
-        // This blog has more than 1 category so hashone_categorized_blog should return true.
-        return true;
-    } else {
-        // This blog has only 1 category so hashone_categorized_blog should return false.
-        return false;
-    }
-}
-
-/**
- * Flush out the transients used in hashone_categorized_blog.
- */
-function hashone_category_transient_flusher() {
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return;
-    }
-    // Like, beat it. Dig?
-    delete_transient('hashone_categories');
-}
-
-add_action('edit_category', 'hashone_category_transient_flusher');
-add_action('save_post', 'hashone_category_transient_flusher');
